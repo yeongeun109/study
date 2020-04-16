c07fc2541f8bf837ab8c282b79bf690df4d4711d
yeongeun109 <62532878+yeongeun109@users.noreply.github.com>
2020-04-16 14:51:59 +0900

    1954달팽이숫자

--- SWEA/SWEA/1954달팽이숫자.cpp
+++ SWEA/SWEA/1954달팽이숫자.cpp
@@ -8,7 +8,60 @@ int main() {
 	for (int i = 0; i < T; i++) {
 		int N;
 		cin >> N;
-		int count = N;
-		int arr[N][N]
+		int num = N;
+		int arr[11][11];
+		
+		int a = 0, b = N-1, cnt = 1;
+
+		for (int j = 0; j < num; j++) {
+			arr[a][j] = cnt;
+			cnt++;
+		}
+		//a++;
+		num--;
+		while (cnt <= N * N) {
+			for (int j = 0; j < num; j++) {
+				a++;
+				arr[a][b] = cnt;
+				cnt++;
+
+				if (cnt == N * N)
+					break;
+			} //1번
+
+			for (int j = 0; j < num; j++) {
+				b--;
+				arr[a][b] = cnt;
+				cnt++;
+				if (cnt == N * N)
+					break;
+			} //2번
+			num--;
+
+			for (int j = 0; j < num; j++) {
+				a--;
+				arr[a][b] = cnt;
+				cnt++;
+				if (cnt == N * N)
+					break;
+			}//3번
+
+			for (int j = 0; j < num; j++) {
+				b++;
+				arr[a][b] = cnt;
+				cnt++;
+				if (cnt == N * N)
+					break;
+			}//4번
+			num--;
+		}
+		
+		cout << '#' << i + 1 << endl;
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				cout << arr[j][k] << ' ';
+			}
+			cout << endl;
+		}
 	}
 }
