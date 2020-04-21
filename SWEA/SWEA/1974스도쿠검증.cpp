6de8939804a3dfa618ca1fd9f39cbe44486a716b
yeongeun109 <62532878+yeongeun109@users.noreply.github.com>
2020-04-21 17:53:41 +0900

    1974스도쿠검증 아직 못품ㅠㅠ

---
+++ SWEA/SWEA/1974스도쿠검증.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int arr[9][9] = { 0 };
+		for (int j = 0; j < 9; j++) {
+			for (int k = 0; k < 9; k++) {
+				cin >> arr[j][k];
+			}
+		}
+
+		
+		bool hey = false;
+		int a = 0, b = 0;
+		int result = 1;
+
+		for (int j = 0; j < 9; j++) {
+			int arr2[9] = { 0 };
+			for (int k = 0; k < 9; k++) {
+				arr2[arr[j][k] -1]++;
+
+				if (arr2[arr[j][k]-1] > 1)
+				{
+					result = 0;
+					hey = true;
+					break;
+				}
+			}
+			if (hey == true)
+				break;
+		}
+
+		for (int j = 0; j < 9; j++) {
+			int arr2[9] = { 0 };
+			for (int k = 0; k < 9; k++) {
+				arr2[arr[k][j] - 1]++;
+
+				if (arr2[arr[k][j]] -1> 1)
+				{
+					result = 0;
+					hey = true;
+					break;
+				}
+			}
+			if (hey == true)
+				break;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				int sum = 0;
+				for (int k = 0; k < 3; k++) {
+					for (int l = 0; l < 3; l++) {
+						sum += arr[(i*3) + k][j * 3 + l];
+					}
+				}
+				if (sum != 45) result = 0;
+			}
+		}
+		cout << '#' << i+1 << ' ' << result << endl;
+	}
+}
