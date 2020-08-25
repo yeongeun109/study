39ed656b6b2a4a9dae75c1b579b6a8359bf814ac
yeongeun109 <ounog@naver.com>
2020-08-25 21:26:29 +0900

    톱니바퀴ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ드디어ㅓㅓㅓㅓㅓㅓㅓ

--- SWEA/SWEA/BJ15662.톱니바퀴(2).cpp
+++ SWEA/SWEA/BJ15662.톱니바퀴(2).cpp
@@ -3,12 +3,30 @@
 
 using namespace std;
 
+int* rotate(int direct, int a[]) {
+	if (direct == 1) { //오른쪽으로 돌리기
+		int temp = a[7];
+		for (int i = 7; i >= 1; i--) {
+			a[i] = a[i - 1];
+		}
+		a[0] = temp;
+	}
+	else { //왼쪽
+		int temp = a[0];
+		for (int i = 0; i < 7; i++) {
+			a[i] = a[i + 1];
+		}
+		a[7] = temp;
+	}
+	return a;
+}
+
 int main() {
 	int T; //톱니바퀴 개수
 	cin >> T;
 	vector <vector<int>> a;
 
-	for (int i = 0; i < T; i++) {
+	for (int i = 0; i < T; i++) { //톱니 배열 저장
 		vector <int> b;
 		string num;
 		cin >> num;
@@ -20,37 +38,87 @@ int main() {
 
 	int K; //회전 횟수
 	cin >> K;
-	int topni_num, direct;
-	for (int i = 0; i < K; i++) {
-		cin >> topni_num >> direct;
-		vector <bool> arr_connect;
-		vector <char> arr_direct;
+	int topni_num, direct; //돌릴 톱니, 방향
+	
 
+	int cnt = 0;
+
+	for (int i = 0; i < K; i++) {
+		vector <bool> arr_connect; //톱니끼리 연결여부
+		cin >> topni_num >> direct;
+		int rotate_topni = topni_num -1;
+		int arr_rotate[8], arr_temp[8];
+		
+		//톱니끼리 연결 배열 초기화
 		for (int j = 0; j < T - 1; j++) {
-			if (a[j][2] == a[j][6])
-				arr_connect[j] = true;
+			if (a[j][2] == a[j + 1][6])
+				arr_connect.push_back(false);
 			else
-				arr_connect[j] = false;
+				arr_connect.push_back(true);
 		}
 
-		int x = topni_num - 1;
-		while (arr_connect[x] != true) {
-			if (direct == 1) { // 오른쪽으로 한 칸 이동
-				for (int j = 0; j < 8; j++) {
-					arr_direct[j] =
-				}
-			}
-			else
+		for (int j = 0; j < 8; j++) {
+			arr_rotate[j] = a[rotate_topni][j];
+		}
+		rotate(direct, arr_rotate);
 
-				x--;
+		for (int j = 0; j < 8; j++) {
+			a[rotate_topni][j] = arr_rotate[j];
+		}
+		//오른쪽 탐색
+		if (rotate_topni <= T - 2) {
+			int temp_direct = direct;
+			while (arr_connect[rotate_topni] == true) {
+				
+				temp_direct *= -1;
+
+				for (int j = 0; j < 8; j++) {
+					arr_temp[j] = a[rotate_topni + 1][j];
+				}
+
+				rotate(temp_direct, arr_temp);
+
+				for (int j = 0; j < 8; j++) {
+					a[rotate_topni + 1][j] = arr_temp[j];
+				}
+
+				rotate_topni += 1;
+
+				if (rotate_topni >= T - 1)
+					break;
+			}
+		}
+
+		rotate_topni = topni_num - 2;
+
+		//왼쪽 탐색
+		if (rotate_topni >= 0) {
+			int temp_direct = direct;
+			while (arr_connect[rotate_topni] == true) {
+				
+				temp_direct *= -1;
+				for (int j = 0; j < 8; j++) {
+					arr_temp[j] = a[rotate_topni][j];
+				}
+				rotate(temp_direct, arr_temp);
+
+				for (int j = 0; j < 8; j++) {
+					a[rotate_topni][j] = arr_temp[j];
+				}
+				
+				rotate_topni -= 1;
+
+				if (rotate_topni < 0)
+					break;
+			}
 		}
 	}
-}
 
-/*int temp;
-				temp = a[x][7];
-				for (int j = 1; j < 7; j++) {
-					a[x][j] = a[x][j + 1];
-				}
-				a[x][0] = temp;
-				*/
+	int sero;
+	sero = a.size();
+	for (int i = 0; i < sero; i++) {
+		if (a[i][0] == 1)
+			cnt++;
+	}
+	cout << cnt;
+}
