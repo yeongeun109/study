85342fc9f97373018233aa45c81ef9a0107bc6b7
yeongeun109 <62532878+yeongeun109@users.noreply.github.com>
2020-11-25 03:08:38 +0900

    bj 1065 한수

--- SWEA/SWEA/1065한수.cpp
+++ SWEA/SWEA/1065한수.cpp
@@ -6,9 +6,19 @@ int hansoo(int n) {
 	int cnt = 0;
 
 	for (int i = 1; i <= n; i++) {
-
+		if (i < 100) {
+			cnt++;
+		}
+		else {
+			if (i / 100 - (i - (i/100)*100)/10 == (i - (i / 100) * 100) / 10 - i % 10)
+				cnt++;
+		}
 	}
+
+	return cnt;
 }
 int main() {
-
+	int n;
+	cin >> n;
+	cout << hansoo(n);
 }
